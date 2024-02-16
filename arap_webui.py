feabe9a9361e047a41788431b2d71ce64ac803f5
Dmitry <mitya.shabat@gmail.com>
2024-02-16 20:46:33 +0400

    Rename numpy alias from onp to np

--- arap_webui.py
+++ arap_webui.py
@@ -5,7 +5,7 @@ import time
 from pathlib import Path
 from typing import List
 
-import numpy as onp
+import numpy as np
 import trimesh.creation
 import trimesh.ray
 import viser
@@ -28,7 +28,7 @@ def main(mesh_path: str):
     mesh_handle = server.add_mesh_trimesh(
         name="/mesh",
         mesh=mesh,
-        wxyz=tf.SO3.from_x_radians(onp.pi / 2).wxyz,
+        wxyz=tf.SO3.from_x_radians(np.pi / 2).wxyz,
         position=(0.0, 0.0, 0.0),
     )
 
@@ -47,8 +47,8 @@ def main(mesh_path: str):
             # Note that mesh is in the mesh frame, so we need to transform the ray.
             R_world_mesh = tf.SO3(mesh_handle.wxyz)
             R_mesh_world = R_world_mesh.inverse()
-            origin = (R_mesh_world @ onp.array(message.ray_origin)).reshape(1, 3)
-            direction = (R_mesh_world @ onp.array(message.ray_direction)).reshape(1, 3)
+            origin = (R_mesh_world @ np.array(message.ray_origin)).reshape(1, 3)
+            direction = (R_mesh_world @ np.array(message.ray_direction)).reshape(1, 3)
             intersector = trimesh.ray.ray_triangle.RayMeshIntersector(mesh)
             hit_pos, _, _ = intersector.intersects_location(origin, direction)
 
@@ -60,7 +60,7 @@ def main(mesh_path: str):
             server.remove_scene_click_callback(scene_click_cb)
 
             # Get the first hit position (based on distance from the ray origin).
-            hit_pos = min(hit_pos, key=lambda x: onp.linalg.norm(x - origin))
+            hit_pos = min(hit_pos, key=lambda x: np.linalg.norm(x - origin))
 
             # Create a sphere at the hit location.
             hit_pos_mesh = trimesh.creation.icosphere(radius=0.1)
