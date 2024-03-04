233050945d60d246a01ec79d83ee67c13d92c062
Dmitry <mitya.shabat@gmail.com>
2024-03-04 21:37:37 +0400

    Load both initial image dataset and the results

--- setup_scripts/load_splat_data.sh
+++ setup_scripts/load_splat_data.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/datasets/input/tandt_db.zip
+wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/evaluation/images.zip
 mkdir -p data
 mkdir -p data/splats
+mkdir -p data/images
 unzip tandt_db.zip -d data/splats
+unzip images.zip -d data/images
