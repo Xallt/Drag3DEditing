0048578d3a0c4d0164a374d70221204f19971b57
Dmitry Shabat <mitya.shabat@gmail.com>
2024-03-07 10:26:31 +0000

    Remove obsolete setup scripts

--- setup_scripts/download_armadillo.sh
+++
@@ -1,2 +0,0 @@
-wget http://graphics.stanford.edu/pub/3Dscanrep/armadillo/Armadillo.ply.gz -O assets/Armadillo.ply.gz
-gzip -d assets/Armadillo.ply.gz
--- setup_scripts/load_splat_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/datasets/input/tandt_db.zip
-wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/evaluation/images.zip
-mkdir -p data
-mkdir -p data/splats
-mkdir -p data/images
-unzip tandt_db.zip -d data/splats
-unzip images.zip -d data/images
