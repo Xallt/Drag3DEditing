82baf6d73cb96ac989c43d1937083d6429850ad1
Dmitry Shabat <mitya.shabat@gmail.com>
2024-05-04 13:21:03 +0000

    Fix comment

--- drag_editing/gaussian_dragging_pipeline.py
+++ drag_editing/gaussian_dragging_pipeline.py
@@ -395,7 +395,7 @@ class GaussianDraggingPipeline:
                 # f1_patch = interpolate_feature_patch(F1, pi[0] + di[0], pi[1] + di[1], args.r_m)
                 loss += ((2 * self.args.r_m + 1) ** 2) * F.l1_loss(f0_patch, f1_patch)
             if type(loss) is float:
-                "no valid tracking point"
+                #no valid tracking point
                 continue
 
             # masked region must stay unchanged
